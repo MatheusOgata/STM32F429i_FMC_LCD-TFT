1e60fd5b3c487ff6c0a3b1a2cf3997cdb95bc145
Matheus Ogata <matheus.ogata@outlook.com>
2021-04-10 12:08:50 -0300

    First commit, Changing the images by pressing the user push-button. Using two framebuffers

---
+++ LCD-TFT_FMC/STM32CubeIDE/Debug/Application/User/Core/subdir.mk
@@ -0,0 +1,44 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Application/User/Core/Driver_9341.c \
+C:/Github/STM32F429i_FMC_LCD-TFT/LCD-TFT_FMC/Core/Src/main.c \
+C:/Github/STM32F429i_FMC_LCD-TFT/LCD-TFT_FMC/Core/Src/stm32f4xx_hal_msp.c \
+C:/Github/STM32F429i_FMC_LCD-TFT/LCD-TFT_FMC/Core/Src/stm32f4xx_it.c \
+../Application/User/Core/syscalls.c \
+../Application/User/Core/sysmem.c 
+
+OBJS += \
+./Application/User/Core/Driver_9341.o \
+./Application/User/Core/main.o \
+./Application/User/Core/stm32f4xx_hal_msp.o \
+./Application/User/Core/stm32f4xx_it.o \
+./Application/User/Core/syscalls.o \
+./Application/User/Core/sysmem.o 
+
+C_DEPS += \
+./Application/User/Core/Driver_9341.d \
+./Application/User/Core/main.d \
+./Application/User/Core/stm32f4xx_hal_msp.d \
+./Application/User/Core/stm32f4xx_it.d \
+./Application/User/Core/syscalls.d \
+./Application/User/Core/sysmem.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Application/User/Core/Driver_9341.o: ../Application/User/Core/Driver_9341.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DDEBUG -DSTM32F429xx -c -I../../Core/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Application/User/Core/Driver_9341.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Application/User/Core/main.o: C:/Github/STM32F429i_FMC_LCD-TFT/LCD-TFT_FMC/Core/Src/main.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DDEBUG -DSTM32F429xx -c -I../../Core/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Application/User/Core/main.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Application/User/Core/stm32f4xx_hal_msp.o: C:/Github/STM32F429i_FMC_LCD-TFT/LCD-TFT_FMC/Core/Src/stm32f4xx_hal_msp.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DDEBUG -DSTM32F429xx -c -I../../Core/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Application/User/Core/stm32f4xx_hal_msp.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Application/User/Core/stm32f4xx_it.o: C:/Github/STM32F429i_FMC_LCD-TFT/LCD-TFT_FMC/Core/Src/stm32f4xx_it.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DDEBUG -DSTM32F429xx -c -I../../Core/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Application/User/Core/stm32f4xx_it.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Application/User/Core/syscalls.o: ../Application/User/Core/syscalls.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DDEBUG -DSTM32F429xx -c -I../../Core/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Application/User/Core/syscalls.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Application/User/Core/sysmem.o: ../Application/User/Core/sysmem.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DDEBUG -DSTM32F429xx -c -I../../Core/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Application/User/Core/sysmem.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
---
+++ LCD-TFT_FMC/STM32CubeIDE/Debug/Drivers/STM32F4xx_HAL_Driver/subdir.mk
@@ -0,0 +1,119 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+C:/Github/STM32F429i_FMC_LCD-TFT/LCD-TFT_FMC/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c \
+C:/Github/STM32F429i_FMC_LCD-TFT/LCD-TFT_FMC/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c \
+C:/Github/STM32F429i_FMC_LCD-TFT/LCD-TFT_FMC/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c \
+C:/Github/STM32F429i_FMC_LCD-TFT/LCD-TFT_FMC/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c \
+C:/Github/STM32F429i_FMC_LCD-TFT/LCD-TFT_FMC/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dsi.c \
+C:/Github/STM32F429i_FMC_LCD-TFT/LCD-TFT_FMC/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c \
+C:/Github/STM32F429i_FMC_LCD-TFT/LCD-TFT_FMC/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c \
+C:/Github/STM32F429i_FMC_LCD-TFT/LCD-TFT_FMC/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c \
+C:/Github/STM32F429i_FMC_LCD-TFT/LCD-TFT_FMC/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c \
+C:/Github/STM32F429i_FMC_LCD-TFT/LCD-TFT_FMC/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c \
+C:/Github/STM32F429i_FMC_LCD-TFT/LCD-TFT_FMC/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_ltdc.c \
+C:/Github/STM32F429i_FMC_LCD-TFT/LCD-TFT_FMC/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_ltdc_ex.c \
+C:/Github/STM32F429i_FMC_LCD-TFT/LCD-TFT_FMC/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c \
+C:/Github/STM32F429i_FMC_LCD-TFT/LCD-TFT_FMC/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c \
+C:/Github/STM32F429i_FMC_LCD-TFT/LCD-TFT_FMC/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c \
+C:/Github/STM32F429i_FMC_LCD-TFT/LCD-TFT_FMC/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c \
+C:/Github/STM32F429i_FMC_LCD-TFT/LCD-TFT_FMC/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_sdram.c \
+C:/Github/STM32F429i_FMC_LCD-TFT/LCD-TFT_FMC/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_spi.c \
+C:/Github/STM32F429i_FMC_LCD-TFT/LCD-TFT_FMC/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c \
+C:/Github/STM32F429i_FMC_LCD-TFT/LCD-TFT_FMC/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c \
+C:/Github/STM32F429i_FMC_LCD-TFT/LCD-TFT_FMC/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_fmc.c 
+
+OBJS += \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal.o \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_cortex.o \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_dma.o \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_dma_ex.o \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_dsi.o \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_exti.o \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_flash.o \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_flash_ex.o \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_flash_ramfunc.o \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_gpio.o \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_ltdc.o \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_ltdc_ex.o \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_pwr.o \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_pwr_ex.o \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_rcc.o \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_rcc_ex.o \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_sdram.o \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_spi.o \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_tim.o \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_tim_ex.o \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_ll_fmc.o 
+
+C_DEPS += \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal.d \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_cortex.d \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_dma.d \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_dma_ex.d \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_dsi.d \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_exti.d \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_flash.d \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_flash_ex.d \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_flash_ramfunc.d \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_gpio.d \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_ltdc.d \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_ltdc_ex.d \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_pwr.d \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_pwr_ex.d \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_rcc.d \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_rcc_ex.d \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_sdram.d \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_spi.d \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_tim.d \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_tim_ex.d \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_ll_fmc.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal.o: C:/Github/STM32F429i_FMC_LCD-TFT/LCD-TFT_FMC/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DDEBUG -DSTM32F429xx -c -I../../Core/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_cortex.o: C:/Github/STM32F429i_FMC_LCD-TFT/LCD-TFT_FMC/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DDEBUG -DSTM32F429xx -c -I../../Core/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_cortex.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_dma.o: C:/Github/STM32F429i_FMC_LCD-TFT/LCD-TFT_FMC/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DDEBUG -DSTM32F429xx -c -I../../Core/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_dma.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_dma_ex.o: C:/Github/STM32F429i_FMC_LCD-TFT/LCD-TFT_FMC/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DDEBUG -DSTM32F429xx -c -I../../Core/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_dma_ex.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_dsi.o: C:/Github/STM32F429i_FMC_LCD-TFT/LCD-TFT_FMC/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dsi.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DDEBUG -DSTM32F429xx -c -I../../Core/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_dsi.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_exti.o: C:/Github/STM32F429i_FMC_LCD-TFT/LCD-TFT_FMC/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DDEBUG -DSTM32F429xx -c -I../../Core/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_exti.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_flash.o: C:/Github/STM32F429i_FMC_LCD-TFT/LCD-TFT_FMC/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DDEBUG -DSTM32F429xx -c -I../../Core/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_flash.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_flash_ex.o: C:/Github/STM32F429i_FMC_LCD-TFT/LCD-TFT_FMC/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DDEBUG -DSTM32F429xx -c -I../../Core/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_flash_ex.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_flash_ramfunc.o: C:/Github/STM32F429i_FMC_LCD-TFT/LCD-TFT_FMC/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DDEBUG -DSTM32F429xx -c -I../../Core/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_flash_ramfunc.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_gpio.o: C:/Github/STM32F429i_FMC_LCD-TFT/LCD-TFT_FMC/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DDEBUG -DSTM32F429xx -c -I../../Core/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_gpio.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_ltdc.o: C:/Github/STM32F429i_FMC_LCD-TFT/LCD-TFT_FMC/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_ltdc.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DDEBUG -DSTM32F429xx -c -I../../Core/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_ltdc.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_ltdc_ex.o: C:/Github/STM32F429i_FMC_LCD-TFT/LCD-TFT_FMC/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_ltdc_ex.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DDEBUG -DSTM32F429xx -c -I../../Core/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_ltdc_ex.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_pwr.o: C:/Github/STM32F429i_FMC_LCD-TFT/LCD-TFT_FMC/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DDEBUG -DSTM32F429xx -c -I../../Core/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_pwr.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_pwr_ex.o: C:/Github/STM32F429i_FMC_LCD-TFT/LCD-TFT_FMC/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DDEBUG -DSTM32F429xx -c -I../../Core/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_pwr_ex.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_rcc.o: C:/Github/STM32F429i_FMC_LCD-TFT/LCD-TFT_FMC/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DDEBUG -DSTM32F429xx -c -I../../Core/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_rcc.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_rcc_ex.o: C:/Github/STM32F429i_FMC_LCD-TFT/LCD-TFT_FMC/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DDEBUG -DSTM32F429xx -c -I../../Core/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_rcc_ex.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_sdram.o: C:/Github/STM32F429i_FMC_LCD-TFT/LCD-TFT_FMC/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_sdram.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DDEBUG -DSTM32F429xx -c -I../../Core/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_sdram.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_spi.o: C:/Github/STM32F429i_FMC_LCD-TFT/LCD-TFT_FMC/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_spi.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DDEBUG -DSTM32F429xx -c -I../../Core/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_spi.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_tim.o: C:/Github/STM32F429i_FMC_LCD-TFT/LCD-TFT_FMC/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DDEBUG -DSTM32F429xx -c -I../../Core/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_tim.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_tim_ex.o: C:/Github/STM32F429i_FMC_LCD-TFT/LCD-TFT_FMC/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DDEBUG -DSTM32F429xx -c -I../../Core/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_tim_ex.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Drivers/STM32F4xx_HAL_Driver/stm32f4xx_ll_fmc.o: C:/Github/STM32F429i_FMC_LCD-TFT/LCD-TFT_FMC/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_fmc.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DDEBUG -DSTM32F429xx -c -I../../Core/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Drivers/STM32F4xx_HAL_Driver/stm32f4xx_ll_fmc.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
